341ab0573637432619d87d711f69caadb98bd33a
the-other-flaneur <66705993+the-other-flaneur@users.noreply.github.com>
2024-12-13 11:52:42 -0300

    make canvas responsive

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,37 @@
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const boundingBox = canvas.getBoundingClientRect();
-canvas.width = boundingBox.width;
-canvas.height = boundingBox.height;
-
-console.log(boundingBox);
-
+const canvas = document.getElementById('game') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+ctx.fillStyle = 'white';
 
 class game {
     ctx: CanvasRenderingContext2D;
 
     constructor(ctx: CanvasRenderingContext2D) {
         this.ctx = ctx;
+        window.addEventListener('resize', () => this.resize());
+        requestAnimationFrame(() => this.draw());
+        this.resize();
     }
 
     draw() {
+        console.log('drawing game');
         this.ctx.fillStyle = 'black';
-        this.ctx.fillRect(0, 0, 800, 600);
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(10, 10, this.ctx.canvas.width - 20, this.ctx.canvas.height - 20);
+        requestAnimationFrame(() => this.draw());
+    }
+
+    resize() {
+        const boundingBox = canvas.parentElement!.getBoundingClientRect();
+        const pixelRatio = window.devicePixelRatio;
+
+        this.ctx.canvas.width = boundingBox.width * pixelRatio;
+        this.ctx.canvas.height = boundingBox.height * pixelRatio;
+        this.ctx.canvas.style.width = `${boundingBox.width}px`;
+        this.ctx.canvas.style.height = `${boundingBox.height}px`;
     }
 }
+
+const gameInstance = new game(ctx);
+gameInstance.draw();
